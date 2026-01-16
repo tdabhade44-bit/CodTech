160a2a1c9ac40830ebb76e9abf2ed64a70d4ff6f
tdabhade44-bit <tdabhade44@gmail.com>
2026-01-16 13:55:06 +0530

    Add files via upload

---
+++ tasks3/chatbot.py
@@ -0,0 +1,28 @@
+import nltk
+from nltk.tokenize import word_tokenize
+
+# Download tokenizer
+nltk.download('punkt')
+
+print("Chatbot: Hello! Type 'bye' to exit.")
+
+while True:
+    user_input = input("You: ").lower()
+
+    tokens = word_tokenize(user_input)
+
+    if "hello" in tokens or "hi" in tokens:
+        print("Chatbot: Hello! How can I help you?")
+    
+    elif "name" in tokens:
+        print("Chatbot: I am an AI Chatbot built using NLP.")
+    
+    elif "help" in tokens:
+        print("Chatbot: I can answer basic questions. Try asking about my name.")
+    
+    elif "bye" in tokens:
+        print("Chatbot: Goodbye! Have a nice day 😊")
+        break
+    
+    else:
+        print("Chatbot: Sorry, I didn't understand that.")
